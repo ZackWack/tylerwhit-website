3b5ecc8e812535b7de5c83c64c5870ab1d373190
ZackWack <121911924+ZackWack@users.noreply.github.com>
2023-01-03 18:19:36 -0700

    Add files via upload

---
+++ like_button.js
@@ -0,0 +1,38 @@
+// 'use strict';
+
+// const e = React.createElement;
+
+// const {useState, useEffect} = React;
+
+// function LikeButton() {
+//   const [liked, setLiked] = useState(false);
+//   return e('button', { onClick: () => setLiked(true) },   
+//     liked ? 'You Like this..' : 'Like');
+// };
+
+// function A(){
+//   return <h1>Hello</h1>
+// }
+
+// // function LikeButton() {
+  
+// //   return (e('h1', {}, 'Hello'), e('h1', {}, 'Hello'));
+// // };
+
+
+// // const domContainer = document.querySelector('#photo_button');
+// // ReactDOM.render(<A/>, domContainer);
+
+
+// const domContainer = document.querySelector('#photo_button');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(<A/>);
+
+// // function FavoriteColor() {
+// //   const [color, setColor] = useState("red");
+
+// //   return <h1>My favorite color is {color}!</h1>
+// // }
+
+// // const domContainer = document.querySelector('#photo_button');
+// // ReactDOM.render(e(FavoriteColor), domContainer);
